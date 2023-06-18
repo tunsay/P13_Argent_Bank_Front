d1c6c6c43a35b86f0bd44446d0a0bee706e84512
tunsay <tuna1997@hotmail.fr>
2023-06-18 22:30:33 +0200

    Update App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+/* router */
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+/* styles */
+import './App.css'
+/* layout */
+import { Layout } from './layout/Layout/Layout'
+/* pages */
+import { Home } from './pages/Home/Home'
+import { Account } from './pages/Account/Account'
+import { Login } from './pages/Login/Login'
+/* componenents */
+import { Authentification } from './components/Authentification/Authentification'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Navigate to="/home" />} />
+            <Route path="/home" element={<Home />} />
+            <Route
+              path="/account"
+              element={<Authentification Child={Account} />}
+            />
+            <Route path="/login" element={<Authentification Child={Login} />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </React.Fragment>
+  )
 }
 
-export default App;
+export default App

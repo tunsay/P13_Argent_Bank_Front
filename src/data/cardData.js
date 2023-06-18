4f780517fcae725e3f45c5951aea0bbe4b292d12
tunsay <tuna1997@hotmail.fr>
2023-06-18 22:33:16 +0200

    Add data for home page and account page

---
+++ src/data/cardData.js
@@ -0,0 +1,21 @@
+import iconChat from '../assets/images/icon-chat.png'
+import iconMoney from '../assets/images/icon-money.png'
+import iconSecurity from '../assets/images/icon-security.png'
+
+export const cardContent = [
+  {
+    icon: iconChat,
+    title: 'You are our #1 priority',
+    text: 'Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.',
+  },
+  {
+    icon: iconMoney,
+    title: 'More savings means higher rates',
+    text: 'The more you save with us, the higher your interest rate will be!',
+  },
+  {
+    icon: iconSecurity,
+    title: 'Security you can trust',
+    text: 'We use top of the line encryption to make sure your data and money is always safe.',
+  },
+]
